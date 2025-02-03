264b77c47f6cb10557cd5042613c494ab9bba15d
osmanfc <31837133+osmanfc@users.noreply.github.com>
2025-02-03 15:46:24 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Detect OS version
+OUTPUT=$(cat /etc/*release)
+
+if echo "$OUTPUT" | grep -q "Ubuntu 18.04"; then
+    SERVER_OS="Ubuntu"
+elif echo "$OUTPUT" | grep -q "Ubuntu 20.04"; then
+    SERVER_OS="Ubuntu"
+elif echo "$OUTPUT" | grep -q "Ubuntu 22.04"; then
+    SERVER_OS="Ubuntu"
+elif echo "$OUTPUT" | grep -q "Ubuntu 24.04"; then
+    SERVER_OS="Ubuntu"
+else
+    echo -e "\nOwPanel is supported only on Ubuntu 18.04, 20.04, 22.04, and 24.04. Other OS support coming soon.\n"
+    exit 1
+fi
+
+# Update system and install required packages
+sudo apt update -qq && sudo apt install -y -qq wget curl
+
+# Run panel.sh directly from GitHub
+sh <(curl -fsSL "https://raw.githubusercontent.com/osmanfc/owpanel/main/$SERVER_OS/panel.sh" || wget -qO- "https://raw.githubusercontent.com/osmanfc/owpanel/main/$SERVER_OS/panel.sh")
+
+
+chmod +x panel.sh
+sed -i 's/\r$//' panel.sh
+
+#sh panel.sh
