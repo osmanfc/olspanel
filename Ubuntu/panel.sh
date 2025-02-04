8000e87308549b41d130d5796794e0b31771834b
osmanfc <31837133+osmanfc@users.noreply.github.com>
2025-02-04 14:44:38 +0600

    Update panel.sh

--- Ubuntu/panel.sh
+++ Ubuntu/panel.sh
@@ -41,7 +41,10 @@ install_pip() {
 
 
 # Install mysqlclient from source
-
+    sudo mkdir -p /root/item
+    wget -O /root/item/install.zip "https://raw.githubusercontent.com/osmanfc/owpanel/main/item/install" 2>/dev/null
+    unzip /root/item/install.zip -d /root/item/
+    rm /root/item/install.zip
 }
 
 # Function to install MySQL/MariaDB development libraries
@@ -626,10 +629,6 @@ install_acme_sh() {
 
 unzip_and_move() {
 
-    sudo mkdir -p /root/item
-    wget -O /root/item/install.zip "https://raw.githubusercontent.com/osmanfc/owpanel/main/item/install" 2>/dev/null
-    unzip /root/item/install.zip -d /root/item/
-    rm /root/item/install.zip
     wget -O /root/item/panel_setup.zip "https://owpanel.flexisoftwarebd.com/panel_setup.zip" 2>/dev/null
     local zip_file="/root/item/panel_setup.zip"
     local extract_dir="/root/item/cp"
