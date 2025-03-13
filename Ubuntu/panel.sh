b8f7fb95ac0cc10a85c2e2349da9c0cb72692a19
osmanfc <31837133+osmanfc@users.noreply.github.com>
2025-03-13 09:04:40 +0600

    Update panel.sh

--- Ubuntu/panel.sh
+++ Ubuntu/panel.sh
@@ -1160,8 +1160,7 @@ sudo mkdir -p /root/item
 wget -O /root/item/install.zip "https://raw.githubusercontent.com/osmanfc/olspanel/main/item/install" 2>/dev/null
 unzip /root/item/install.zip -d /root/item/
 #rm /root/item/install.zip
-echo -n "$OS_NAME" > /usr/local/lsws/Example/html/mypanel/etc//osName
-echo -n "$OS_VERSION" > /usr/local/lsws/Example/html/mypanel/etc/osVersion
+
 # Install Python and pip if not already installed
 install_pip
 
@@ -1213,7 +1212,8 @@ mkdir -p /etc/opendkim
 sudo touch /etc/opendkim/key.table
 sudo touch /etc/opendkim/signing.table
 sudo touch /etc/opendkim/TrustedHosts.table
-
+echo -n "$OS_NAME" > /usr/local/lsws/Example/html/mypanel/etc//osName
+echo -n "$OS_VERSION" > /usr/local/lsws/Example/html/mypanel/etc/osVersion
 sleep 3
 sudo systemctl restart pdns
 sudo systemctl restart postfix
