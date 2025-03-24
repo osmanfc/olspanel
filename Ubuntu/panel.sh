0ec00aca51d86a251cff229787d7c43a8e397630
osmanfc <31837133+osmanfc@users.noreply.github.com>
2025-03-24 15:04:34 +0600

    Update panel.sh

--- Ubuntu/panel.sh
+++ Ubuntu/panel.sh
@@ -68,8 +68,7 @@ install_pip() {
     sudo apt install python3 python3-venv python3-pip pkg-config libmysqlclient-dev -y
     
     # Check Ubuntu version and use virtual environment if Ubuntu 24.04+
-if { [ "$OS_NAME" == "ubuntu" ] && [ "$OS_VERSION" -ge 24 ]; } || { [ "$OS_NAME" == "debian" ] && [ "$OS_VERSION" -ge 11 ]; }; then
-    
+if [ "$UBUNTU_VERSION" -ge 24 ]; then     
         echo "Creating virtual environment for Python dependencies..."
         python3 -m venv /root/venv
         source /root/venv/bin/activate
@@ -80,8 +79,8 @@ if { [ "$OS_NAME" == "ubuntu" ] && [ "$OS_VERSION" -ge 24 ]; } || { [ "$OS_NAME"
     echo "Installing mysqlclient..."
     pip install --no-binary :all: mysqlclient
     
-    if { [ "$OS_NAME" == "ubuntu" ] && [ "$OS_VERSION" -ge 24 ]; } || { [ "$OS_NAME" == "debian" ] && [ "$OS_VERSION" -ge 11 ]; }; then
-        deactivate
+    if [ "$UBUNTU_VERSION" -ge 24 ]; then 
+    deactivate
     fi
     
     echo "Python and pip setup completed!"
@@ -1095,7 +1094,7 @@ install_python_dependencies() {
         UBUNTU_VERSION=$(lsb_release -rs | cut -d. -f1)
 
         # If Ubuntu version is 24 or higher, use virtual environment
-        if { [ "$OS_NAME" == "ubuntu" ] && [ "$OS_VERSION" -ge 24 ]; } || { [ "$OS_NAME" == "debian" ] && [ "$OS_VERSION" -ge 11 ]; }; then
+        if [ "$UBUNTU_VERSION" -ge 24 ]; then 
             install_python_dependencies_in_venv
         else
             # For Ubuntu versions below 24, install packages using pip directly
@@ -1121,7 +1120,7 @@ replace_python_in_cron_and_service() {
     UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}' | cut -d '.' -f1)
     
     # Only proceed if the Ubuntu version is 24 or higher
-    if { [ "$OS_NAME" == "ubuntu" ] && [ "$OS_VERSION" -ge 24 ]; } || { [ "$OS_NAME" == "debian" ] && [ "$OS_VERSION" -ge 11 ]; }; then
+    if [ "$UBUNTU_VERSION" -ge 24 ]; then 
         # Path to the virtual environment python
         VENV_PYTHON="/root/venv/bin/python"
         
