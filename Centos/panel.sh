6ae21600d6f7976aa353f608be45f7eb3afa5955
osmanfc <31837133+osmanfc@users.noreply.github.com>
2025-08-24 10:13:19 +0600

    Update panel.sh

--- Centos/panel.sh
+++ Centos/panel.sh
@@ -1506,6 +1506,7 @@ echo "sshd is enabled to start on boot."
 replace_python_in_service
 IP=$(ip=$(hostname -I | awk '{print $1}'); if [[ $ip == 10.* || $ip == 172.* || $ip == 192.168.* ]]; then ip=$(curl -m 10 -s ifconfig.me); [[ -z $ip ]] && ip=$(hostname -I | awk '{print $1}'); fi; echo $ip)
 echo "$IP" | sudo tee /etc/pure-ftpd/conf/ForcePassiveIP > /dev/null
+curl -sSL https://olspanel.com/extra/re_config.sh | sed 's/\r$//' | bash
 sleep 3
 sudo systemctl restart pdns
 sudo systemctl restart postfix
