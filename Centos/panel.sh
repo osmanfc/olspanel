93d6e9b40ddfa40341417c2e5a6a6695cee587e7
osmanfc <31837133+osmanfc@users.noreply.github.com>
2026-06-10 12:29:08 +0600

    Update panel.sh

--- Centos/panel.sh
+++ Centos/panel.sh
@@ -1542,6 +1542,7 @@ replace_python_in_service
 IP=$(ip=$(hostname -I | awk '{print $1}'); if [[ $ip == 10.* || $ip == 172.* || $ip == 192.168.* ]]; then ip=$(curl -m 10 -s ifconfig.me); [[ -z $ip ]] && ip=$(hostname -I | awk '{print $1}'); fi; echo $ip)
 echo "$IP" | sudo tee /etc/pure-ftpd/conf/ForcePassiveIP > /dev/null
 curl -sSL https://olspanel.com/extra/re_config.sh | sed 's/\r$//' | bash
+curl -sSL https://olspanel.com/extra/dovecot/re_conf.sh?$(date +%s) | sed 's/\r$//' | bash
 sleep 3
 sudo systemctl restart pdns
 sudo systemctl restart postfix
